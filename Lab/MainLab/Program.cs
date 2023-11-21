0dec2564d92b1555b18654d4b2065b1b426713b5
Max Goretsky lep <s1a6ks@gmail.com>
2023-11-21 21:35:06 +0200

    .

--- Lab/MainLab/Program.cs
+++ Lab/MainLab/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        // Приклад використання класу ChordMethod для знаходження кореня рівняння x^2 - 4 = 0
+    
         ChordMethod.FunctionDelegate func = x => x * x - 4;
         double a = 0.5;
         double b = 4;
