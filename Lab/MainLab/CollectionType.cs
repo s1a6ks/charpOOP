ca65c599bfa8b3bed55f61c8f65bf9bc4470f2cb
Max Goretsky lep <s1a6ks@gmail.com>
2023-12-28 13:59:54 +0200

    .

--- Lab/MainLab/CollectionType.cs
+++ Lab/MainLab/CollectionType.cs
@@ -6,40 +6,40 @@ public class CollectionType<T> : IEnumerable<T>
 {
     private List<T> collection;
 
-    // Конструктор за замовчуванням
+  
     public CollectionType()
     {
         collection = new List<T>();
     }
 
-    // Конструктор, який приймає початковий набір елементів
+   
     public CollectionType(IEnumerable<T> initialItems)
     {
         collection = new List<T>(initialItems);
     }
 
-    // Кількість елементів в колекції
+    
     public int Count => collection.Count;
 
-    // Додавання елемента до колекції
+    
     public void Add(T item)
     {
         collection.Add(item);
     }
 
-    // Видалення елемента з колекції
+    
     public bool Remove(T item)
     {
         return collection.Remove(item);
     }
 
-    // Очищення колекції
+   
     public void Clear()
     {
         collection.Clear();
     }
 
-    // Перевантажений індексатор для отримання та встановлення значень за індексом
+    
     public T this[int index]
     {
         get
@@ -56,13 +56,13 @@ public class CollectionType<T> : IEnumerable<T>
         }
     }
 
-    // Реалізація інтерфейсу IEnumerable<T>
+  
     public IEnumerator<T> GetEnumerator()
     {
         return collection.GetEnumerator();
     }
 
-    // Реалізація інтерфейсу IEnumerable
+   
     IEnumerator IEnumerable.GetEnumerator()
     {
         return collection.GetEnumerator();
